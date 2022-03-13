b6e44e876f3607616a69b992ad90ae3cacbd6414
MarineLLV <marine.lucottelevisage@gmail.com>
2022-03-13 22:56:28 +0100

    check if the player won

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import random
-# Pick a random word and check answers
 
 word_list = ["algorithm", "binary", "coplanar", "deviation", "exponential", "fibonacci", "gaussian", "hexadecimal", "increment", "joule", "kilometer", "logarithmic", "modulo", "number", "operator", "property", "quadratic", "range", "square", "transpose", "unit", "vector"]
 chosen_word = random.choice(word_list)
+print(chosen_word)
 
 display = []
 for _ in range(len(chosen_word)):
     display.append("_")
 print(display)
 
-guess = input("Guess a letter: ").lower()
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-print(display)
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
